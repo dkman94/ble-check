1ce2b70cc4a0fb375d22a27d02c6aced84800e01
dkman94 <deepak.s.kumar.85@gmail.com>
2014-03-06 13:17:59 -0500

    v2
    Commented, removed unneccesary methods and imports, and made more
    readable.

--- src/com/example/ble_check/MainActivity.java
+++ src/com/example/ble_check/MainActivity.java
@@ -1,89 +1,74 @@
 package com.example.ble_check;
 
-import java.util.Locale;
-
 import android.os.Bundle;
-import android.os.Debug;
 import android.app.Activity;
-import android.content.pm.PackageManager;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.TextToSpeech.OnInitListener;
-import android.view.Menu;
 import android.widget.TextView;
 
 public class MainActivity extends Activity implements  OnInitListener{
 	
-	boolean haveble = false;
-	public int gotit = 0;
+	//boolean variable that determines if the phone has Bluetooth LE
+	boolean haveBLE = false;
+	//variable int to determine spoken message
+	public int gotBLE = 0;
+	//create the TextToSpeech variable
 	TextToSpeech talker;
 	  
 	 @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		//intialize the constructor of the TextToSpeech variable
 		talker = new TextToSpeech(this, this);
 		
-	    if (!getPackageManager().hasSystemFeature("android.hardware.bluetooth_le")) {
-	      haveble = false;
-	    }
-	    else
-	    {
-	       haveble = true;
-	    }
+		//Android phone does not have the system feature of Bluetooth LE, then haveBLE is false
+	    	if (!getPackageManager().hasSystemFeature("android.hardware.bluetooth_le")) {
+	    			haveBLE = false;
+	    	}
+	    //else the Phone does have Bluetooth LE, and haveBLE is true
+	    	else {
+	    			haveBLE = true;
+	    	  }
 	    
+	    //create the TextView to print the result
 	    TextView textView1 = (TextView) findViewById(R.id.textView1);
-	    if(haveble == true)
-	    {
-	    	gotit = 1;
-	    	textView1.setText("Wohoo! All the Bluetooth 4.0's!");
-	    	//tts.speak("Woot all the Bluetooths!", TextToSpeech.QUEUE_FLUSH, null);
-	    	say("You've got Bluetooth 4.0!");
-	    }
-	    else {
-	    	gotit = 0;
-	    	textView1.setText("Oh oh sphagettio! You've got no Bluetooth 4.0's! Yikes!");
-	    	//tts.speak("No bluetooth's for you. Sad face", TextToSpeech.QUEUE_FLUSH, null);
-	    	say("Shoot! Your phone does not support Bluetooth 4.0.");
-
-	    }
-	    	
-	 } 
+	    	if(haveBLE == true)  {
+	    		//set gotBLE to true(or 1) and set text view to display success
+	    			gotBLE = 1;
+	    			textView1.setText("Wohoo! All the Bluetooth 4.0's!");
+	    	  }
+	    	else {
+	    		//set gotBLE to true(or 1) and set text view to failure success
+	    			gotBLE = 0;
+	    			textView1.setText("Uh oh sphagettio! You've got no Bluetooth 4.0's! Yikes!");
+	    	  }
+	     } 
 	 
+	 	//say method inputs a string and speaks out the result
+	 	//QUEUE_FLUSH only allows 1 result to be spoken as is required in this app
 	 public void say(String text2say){
 		 talker.speak(text2say,  TextToSpeech.QUEUE_FLUSH, null);
-		 //System.out.println("spoke");
 	 }
 	 
+	 	//onInit method allows result of Bluetooth 4.0 Report to be spoken
 	 public void onInit(int code) {
-		 if(gotit == 1)
-		 {
-		   say("You've got Bluetooth 4.0!");     
-		 }
-		 else
-		 {
-			 say("Darn. Your phone does not support Bluetooth 4.0.");
-		 }
+		 	if(gotBLE == 1)  {
+		 			say("You've got Bluetooth 4.0!");     
+		  }
+		    else {
+		    		say("Darn. Your phone does not support Bluetooth 4.0.");
+		  }
 	}
 	 
+	 	//onDestroy() makes sure to end the spoken result if the app if ended by the system
 	 protected void onDestroy() {
-		 if(talker != null) {
-			 talker.stop();
-			 talker.shutdown();
+		 	if(talker != null) {
+		 		talker.stop();
+		 		talker.shutdown();
 		 }
-		 super.onDestroy();
-	      
+		 super.onDestroy();	      
 	}
-	 
-	 
-	 
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-	
 	
 
 }
