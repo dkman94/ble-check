7265336b98827831a14780a5c84976f1ec4818d6
dkman94 <deepak.s.kumar.85@gmail.com>
2014-03-06 20:58:58 -0500

    v3

--- src/com/example/ble_check/MainActivity.java
+++ src/com/example/ble_check/MainActivity.java
@@ -8,52 +8,51 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity implements  OnInitListener{
 	
-	//boolean variable that determines if the phone has Bluetooth LE
-	boolean haveBLE = false;
+	boolean mHaveBLE = false; //boolean variable that determines if the phone has Bluetooth LE
 	//variable int to determine spoken message
-	public int gotBLE = 0;
+	public int mGotBLE = 0;
 	//create the TextToSpeech variable
-	TextToSpeech talker;
+	TextToSpeech mTalker;
 	  
 	 @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		//intialize the constructor of the TextToSpeech variable
-		talker = new TextToSpeech(this, this);
+		mTalker = new TextToSpeech(this, this);
 		
 		//Android phone does not have the system feature of Bluetooth LE, then haveBLE is false
 	    	if (!getPackageManager().hasSystemFeature("android.hardware.bluetooth_le")) {
-	    			haveBLE = false;
+	    			mHaveBLE = false;
 	    	}
-	    //else the Phone does have Bluetooth LE, and haveBLE is true
+	        //else the Phone does have Bluetooth LE, and haveBLE is true
 	    	else {
-	    			haveBLE = true;
+	    			mHaveBLE = true;
 	    	  }
 	    
 	    //create the TextView to print the result
-	    TextView textView1 = (TextView) findViewById(R.id.textView1);
-	    	if(haveBLE == true)  {
+	    TextView mTextView1 = (TextView) findViewById(R.id.textView1);
+	    	if(mHaveBLE == true)  {
 	    		//set gotBLE to true(or 1) and set text view to display success
-	    			gotBLE = 1;
-	    			textView1.setText("Wohoo! All the Bluetooth 4.0's!");
+	    			mGotBLE = 1;
+	    			mTextView1.setText("Wohoo! All the Bluetooth 4.0's!");
 	    	  }
 	    	else {
 	    		//set gotBLE to true(or 1) and set text view to failure success
-	    			gotBLE = 0;
-	    			textView1.setText("Uh oh sphagettio! You've got no Bluetooth 4.0's! Yikes!");
+	    			mGotBLE = 0;
+	    			mTextView1.setText("Uh oh sphagettio! You've got no Bluetooth 4.0's! Yikes!");
 	    	  }
 	     } 
 	 
 	 	//say method inputs a string and speaks out the result
 	 	//QUEUE_FLUSH only allows 1 result to be spoken as is required in this app
 	 public void say(String text2say){
-		 talker.speak(text2say,  TextToSpeech.QUEUE_FLUSH, null);
+		 mTalker.speak(text2say,  TextToSpeech.QUEUE_FLUSH, null);
 	 }
 	 
 	 	//onInit method allows result of Bluetooth 4.0 Report to be spoken
 	 public void onInit(int code) {
-		if(gotBLE == 1)  {
+		if(mGotBLE == 1)  {
 		 		say("You've got Bluetooth 4.0!");     
 		  }
 		else {
@@ -63,10 +62,10 @@ public class MainActivity extends Activity implements  OnInitListener{
 	 
 	 	//onDestroy() makes sure to end the spoken result if the app if ended by the system
 	 protected void onDestroy() {
-		if(talker != null){
+		if(mTalker != null){
 			//after app has been close by system. Stop the speech and then shut it down
-		 		talker.stop();
-		 		talker.shutdown();
+		 		mTalker.stop();
+		 		mTalker.shutdown();
 		 }
 		 super.onDestroy();	      
 	}
